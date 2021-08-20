fca4d645d4daf8524ae70e465750ba142424c998
Jugg1 <adeoluwaadeboye@gmail.com>
2021-08-20 22:26:21 +0100

    :construction: Added HNG logos and links

--- template/resume.php
+++ template/resume.php
@@ -19,6 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="icon" href="img/hnglogo.png" />
 </head>
 
 <body>
@@ -33,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <?php endif; ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#"><img class="img-fluid rounded align-self-center" src="img/hnglogo.png" alt="Adeoluwa Adeboye" width="30px"></img>HNG/IGX</a>
+            <a class="navbar-brand" href="https://internship.zuri.team"><img class="img-fluid rounded align-self-center" src="img/hnglogo.png" alt="Adeoluwa Adeboye" width="30px"></img>HNG/i4g</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -251,6 +252,8 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                     <p className="mt-4 mb-0">
                         Adeoluwa | Copyright &copy; 2021
                     </p>
+                    <a href="https://internship.zuri.team"><img class="img-fluid rounded align-self-center" src="img/hnglogo.png" alt="Adeoluwa Adeboye" width="30px" style="color: black;"></img> HNG/i4g</a>
+
                 </div>
             </div>
         </div>
